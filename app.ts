02715df295cb976fa34e3593c96cb7d406de0008
soulayman <soulaymanjaa09@gmail.com>
2025-10-28 11:34:06 +0100

    project set UP

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import sessionConfig from './config/session';
+import './config/DB';
+import { graphqlHTTP } from 'express-graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+
+const app = express();
+app.use(express.urlencoded({ extended : true }));
+app.use(session(sessionConfig));
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
