5e224bd5c5527f1c6233386b8b09a378abc74458
soulayman <soulaymanjaa09@gmail.com>
2025-10-29 01:32:14 +0100

    created login method with graphql services and well structured project

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,10 +1,19 @@
 import User from "../models/User";
+import * as AuthService from "../services/authService";
 
-export async function login(email:string ,password:string){
+
+export async function login(email: string, password: string) {
+
+    try {
+
+        const token = await AuthService.login(email, password);
+
+        return {token};
 
 
-let token = "hi"+email+password+"";
-
-return {token};
+    } catch (err:any) {
+        console.log({ error: err.message });
+        // res.status(400).json({ error: err.message });
+    }
 
 }
